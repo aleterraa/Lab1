1650c264a8ac172093b51cdc672ba6c0c8e6f4f6
aurorab97 <aurorabetta.97@gmail.com>
2025-02-20 14:35:22 +0100

    Aggiunte funzioni
    Aggiunte funzioni per il gioco Carta, Sasso, Forbice

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 # PROGETTO CARTA FORBICE SASSO
 
-print("Scegli: Carta (C), Forbice (F) o Sasso (S)")
-scelta1 = input("Giocatore 1 : ")
-scelta2 = input("Giocatore 2 : ")
+    print("Scegli: Carta (C), Forbice (F) o Sasso (S)")
+    scelta = input("Scegli la mossa: ")
+    return scelta
+
+scelta1 = scelta_mossa()
+scelta2 = scelta_mossa()
+
+def gioco(scelta1, scelta2):
+    if scelta1 == scelta2:
+        print("Siete pari! Riscegliere le mosse")
+        scelta1 = scelta_mossa()
+        scelta2 = scelta_mossa()
+        gioco(scelta1, scelta2)
+    elif scelta1 == "C":
+        if scelta2 == "S":
+            print("Carta batte sasso")
+        if scelta2 == "F":
+            print("Forbice batte carta")
+    elif scelta1 == "S":
+        if scelta2 == "C":
+            print("Carta batte sasso")
+        if scelta2 == "F":
+            print("Forbice batte carta")
+    elif scelta1 == "F":
+        if scelta2 == "S":
+            print("Sasso batte forbice")
+        if scelta2 == "C":
+            print("Forbice batte carta")
+
+gioco(scelta1, scelta2)
 
 
