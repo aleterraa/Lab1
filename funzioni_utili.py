f63aa8a7a3720c39b9b9ec76ef580b3d57d5e5f5
aleterraa <aleterra02@hotmail.com>
2025-02-20 15:24:21 +0100

    Creata la libreria di funzioni utili

---
+++ funzioni_utili.py
@@ -0,0 +1,31 @@
+#Libreria di funzioni per giocare a carta, forbice e sasso
+def scelta_mossa():
+    print("Scegli: Carta (C), Forbice (F) o Sasso (S)")
+    scelta = input("Scegli la mossa: ")
+    return scelta
+
+def gioco(scelta1, scelta2):
+    if scelta1 == scelta2:
+        print("Siete pari! Riscegliere le mosse")
+        scelta1 = scelta_mossa()
+        scelta2 = scelta_mossa()
+        gioco(scelta1, scelta2)
+    elif scelta1 == "C":
+        if scelta2 == "S":
+            print("Carta batte sasso")
+        if scelta2 == "F":
+            print("Forbice batte carta")
+    elif scelta1 == "S":
+        if scelta2 == "C":
+            print("Carta batte sasso")
+        if scelta2 == "F":
+            print("Forbice batte carta")
+    elif scelta1 == "F":
+        if scelta2 == "S":
+            print("Sasso batte forbice")
+        if scelta2 == "C":
+            print("Forbice batte carta")
+
+if __name__ == "__main__":
+    print("Questo script è eseguito direttamente")
+    scelta_mossa()
--- main.py
+++ main.py
@@ -1,35 +1,8 @@
 # PROGETTO CARTA FORBICE SASSO
-
-def scelta_mossa():
-    print("Scegli: Carta (C), Forbice (F) o Sasso (S)")
-    scelta = input("Scegli la mossa: ")
-    return scelta
+from funzioni_utili import scelta_mossa
+from funzioni_utili import gioco
 
 scelta1 = scelta_mossa()
 scelta2 = scelta_mossa()
 
-def gioco(scelta1, scelta2):
-    if scelta1 == scelta2:
-        print("Siete pari! Riscegliere le mosse")
-        scelta1 = scelta_mossa()
-        scelta2 = scelta_mossa()
-        gioco(scelta1, scelta2)
-    elif scelta1 == "C":
-        if scelta2 == "S":
-            print("Carta batte sasso")
-        if scelta2 == "F":
-            print("Forbice batte carta")
-    elif scelta1 == "S":
-        if scelta2 == "C":
-            print("Carta batte sasso")
-        if scelta2 == "F":
-            print("Forbice batte carta")
-    elif scelta1 == "F":
-        if scelta2 == "S":
-            print("Sasso batte forbice")
-        if scelta2 == "C":
-            print("Forbice batte carta")
-
 gioco(scelta1, scelta2)
-
-
