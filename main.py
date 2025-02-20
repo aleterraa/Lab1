061231952e57d269c66dc19c7c1c714e220b9627
aurorab97 <aurorabetta.97@gmail.com>
2025-02-20 15:55:13 +0100

    Aggiunto codice nel main
    Implementato il gioco Carta Sasso Forbice

--- main.py
+++ main.py
@@ -2,7 +2,17 @@
 from funzioni_utili import scelta_mossa
 from funzioni_utili import gioco
 
-scelta1 = scelta_mossa()
-scelta2 = scelta_mossa()
+while True:
+    scelta1 = scelta_mossa()
+    scelta2 = scelta_mossa()
+    gioco(scelta1, scelta2)
+    richiesta = input("Vuoi giocare ancora? Sì o No? ")
+    if richiesta == "Sì":
+        scelta1 = scelta_mossa()
+        scelta2 = scelta_mossa()
+        gioco(scelta1, scelta2)
+    elif richiesta == "No":
+        break
+    else:
+        print("Scelta non valida! Scegliere: Sì o No")
 
-gioco(scelta1, scelta2)
